36cf422826b3a01c5081d70ec279a387495c8028
jameswilddev <jameswilddev@outlook.com>
2017-03-30 22:44:02 +0100

    Implemented SplitByWhiteSpace.

---
+++ src/SplitByWhiteSpace.ts
@@ -0,0 +1,27 @@
+type UntypedToken = {
+    StartIndex: number
+    Text: string
+}
+
+function SplitByWhiteSpace(source: string): UntypedToken[] {
+    let token: UntypedToken | undefined = undefined
+    const tokens: UntypedToken[] = []
+
+    // TODO: Is this Unicode safe?
+    for (let i = 0; i < source.length; i++) {
+        const character = source.charAt(i)
+        if (character.trim().length) {
+            if (token) {
+                token.Text += character.toLowerCase()
+            } else {
+                token = {
+                    StartIndex: i,
+                    Text: character.toLowerCase()
+                }
+                tokens.push(token)
+            }
+        } else if (token) token = undefined
+    }
+
+    return tokens
+}
