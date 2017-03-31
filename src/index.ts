35eebe23990aa9ece97b08a38f1674cdee838355
James Wild <James Wild>
2017-03-31 22:41:01 +0100

    Removed direct references to ParseToken helpers.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,2 @@
 /// <reference path="SplitByWhiteSpace.ts" />
-/// <reference path="ParseBoolean.ts" />
-/// <reference path="ParseInteger.ts" />
-/// <reference path="ParseSymbol.ts" />
 /// <reference path="ParseToken.ts" />
