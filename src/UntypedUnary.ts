941987d3259af2169235308421a68d08c3029071
James Wild <James Wild>
2017-04-01 11:52:38 +0100

    Added lookup objects for when it does not matter whether a symbol or keyword was used to reference an operator.

--- src/UntypedUnary.ts
+++ src/UntypedUnary.ts
@@ -8,3 +8,7 @@ const UntypedUnarySymbols: { [symbol: string]: UntypedUnary } = {
 const UntypedUnaryKeywords: { [keyword: string]: UntypedUnary } = {
     "not": "Not"
 }
+
+const UntypedUnaryKeywordsAndSymbols: { [keywordOrSymbol: string]: UntypedUnary } = {}
+for (const symbol in UntypedUnarySymbols) UntypedUnaryKeywordsAndSymbols[symbol] = UntypedUnarySymbols[symbol]
+for (const keyword in UntypedUnaryKeywords) UntypedUnaryKeywordsAndSymbols[keyword] = UntypedUnaryKeywords[keyword]
