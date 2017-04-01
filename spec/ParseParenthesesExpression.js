bc36b725752f5976282fe6f8492c07e4509df797
jameswilddev <jameswilddev@outlook.com>
2017-04-01 22:27:47 +0100

    Switched to ParseExpression for parentheses.

--- spec/ParseParenthesesExpression.js
+++ spec/ParseParenthesesExpression.js
@@ -2,8 +2,10 @@ describe("ParseParenthesesExpression", () => {
     const Namespace = require("rewire")("../dist/index.js")
     const ParseParenthesesExpression = Namespace.__get__("ParseParenthesesExpression")
 
-    Namespace.__set__("TryParseExpression", (tokens) => {
+    Namespace.__set__("ParseExpression", (tokens, startIndex, endIndex) => {
         expect(tokens).toEqual("Test Child Tokens")
+        expect(startIndex).toEqual(253)
+        expect(endIndex).toEqual(284)
         return "Test Parsed Child Expression"
     })
 
@@ -17,48 +19,58 @@ describe("ParseParenthesesExpression", () => {
 
     Test("one non-parentheses", [{
         Type: "Misc A",
-        StartIndex: 253
+        StartIndex: 253,
+        EndIndex: 284
     }], undefined)
 
     Test("multiple non-parentheses", [{
         Type: "Misc A",
-        StartIndex: 253
+        StartIndex: 253,
+        EndIndex: 284
     }, {
         Type: "Misc B",
-        StartIndex: 342
+        StartIndex: 342,
+        EndIndex: 366
     }], undefined)
 
     Test("one parentheses", [{
         Type: "Parentheses",
         StartIndex: 253,
+        EndIndex: 284,
         Contents: "Test Child Tokens"
     }], "Test Parsed Child Expression")
 
     Test("two parentheses", [{
         Type: "Parentheses",
         StartIndex: 253,
+        EndIndex: 284,
         Contents: "Test Child Tokens A"
     }, {
         Type: "Parentheses",
         StartIndex: 342,
+        EndIndex: 366,
         Contents: "Test Child Tokens B"
     }], undefined)
 
     Test("one parentheses, one non-parentheses", [{
         Type: "Parentheses",
         StartIndex: 253,
+        EndIndex: 284,
         Contents: "Test Child Tokens"
     }, {
         Type: "Misc B",
-        StartIndex: 342
+        StartIndex: 342,
+        EndIndex: 366
     }], undefined)
 
     Test("one non-parentheses, one parentheses", [{
         Type: "Misc A",
-        StartIndex: 253
+        StartIndex: 253,
+        EndIndex: 284
     }, {
         Type: "Parentheses",
         StartIndex: 342,
+        EndIndex: 366,
         Contents: "Test Child Token"
     }], undefined)
 })
