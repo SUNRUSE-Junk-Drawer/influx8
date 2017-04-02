bb511228e60630fb723cf84f5ffd05e84683e4e5
jameswilddev <jameswilddev@outlook.com>
2017-04-02 09:22:58 +0100

    Keyword parsing now uses a dictionary lookup. This will make it easier to implement statements.

---
+++ spec/Keywords.js
@@ -0,0 +1,23 @@
+describe("Keywords", () => {
+    const Keywords = require("rewire")("../dist/index.js").__get__("Keywords")
+
+    function Maps(keyword, to) {
+        it("maps \"" + keyword + "\" to \"" + to + "\"", function () {
+            expect(Keywords[keyword]).toEqual(to)
+        })
+    }
+
+    function DoesNotMap(keyword) {
+        it("does not map \"" + keyword + "\"", function () {
+            expect(Keywords[keyword]).toBeUndefined()
+        })
+    }
+
+    DoesNotMap("(")
+    DoesNotMap(")")
+    DoesNotMap("-")
+    DoesNotMap("+")
+    DoesNotMap("!")
+    Maps("and", "Operator")
+    Maps("not", "Operator")
+})
