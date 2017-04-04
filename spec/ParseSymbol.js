2aee0b8d526a2f4dd5c75f2aa19d2cdf3f5e1c67
jameswilddev <jameswilddev@outlook.com>
2017-04-04 20:31:46 +0100

    Improved unit test coverage of ParseSymbol.

--- spec/ParseSymbol.js
+++ spec/ParseSymbol.js
@@ -3,7 +3,8 @@ describe("ParseSymbol", () => {
     const ParseSymbol = Namespace.__get__("ParseSymbol")
     Namespace.__set__("Symbols", {
         ">$%": "Short Symbol",
-        ">$%!?": "Long Symbol"
+        ">$%!?": "Long Symbol",
+        "&&*": "Symbol Similar To Neither"
     })
 
     function Test(description, input, output, tokensToParse) {
@@ -37,11 +38,30 @@ describe("ParseSymbol", () => {
         Symbol: ">$%!?"
     }], fail)
 
+    Test("lone symbol similar to neither", "&&*", [{
+        Type: "Symbol Similar To Neither",
+        StartIndex: 32,
+        EndIndex: 34,
+        Symbol: "&&*"
+    }], fail)
+
     Test("short symbol at start", ">$%matchesnothing", [{
         Type: "Short Symbol",
         StartIndex: 32,
         EndIndex: 34,
         Symbol: ">$%"
+    }, "Misc A", "Misc B"], {
+            "matchesnothing": {
+                StartIndex: 35,
+                Result: ["Misc A", "Misc B"]
+            }
+        })
+
+    Test("symbol similar to neither at start", "&&*matchesnothing", [{
+        Type: "Symbol Similar To Neither",
+        StartIndex: 32,
+        EndIndex: 34,
+        Symbol: "&&*"
     }, "Misc A", "Misc B"], {
             "matchesnothing": {
                 StartIndex: 35,
@@ -85,11 +105,39 @@ describe("ParseSymbol", () => {
             }
         })
 
+    Test("symbol similar to neither at end", "matchesnothing&&*", ["Misc A", "Misc B", {
+        Type: "Symbol Similar To Neither",
+        StartIndex: 46,
+        EndIndex: 48,
+        Symbol: "&&*"
+    }], {
+            "matchesnothing": {
+                StartIndex: 32,
+                Result: ["Misc A", "Misc B"]
+            }
+        })
+
     Test("short symbol in middle", "matches>$%nothing", ["Misc A", "Misc B", {
         Type: "Short Symbol",
         StartIndex: 39,
         EndIndex: 41,
         Symbol: ">$%"
+    }, "Misc C", "Misc D", "Misc E"], {
+            "matches": {
+                StartIndex: 32,
+                Result: ["Misc A", "Misc B"]
+            },
+            "nothing": {
+                StartIndex: 42,
+                Result: ["Misc C", "Misc D", "Misc E"]
+            }
+        })
+
+    Test("symbol similar to neither in middle", "matches&&*nothing", ["Misc A", "Misc B", {
+        Type: "Symbol Similar To Neither",
+        StartIndex: 39,
+        EndIndex: 41,
+        Symbol: "&&*"
     }, "Misc C", "Misc D", "Misc E"], {
             "matches": {
                 StartIndex: 32,
@@ -336,6 +384,232 @@ describe("ParseSymbol", () => {
                 Result: ["Misc A", "Misc B"]
             },
             "hesnoth>$%!?ing": {
+                StartIndex: 41,
+                Result: ["Misc C", "Misc D", "Misc E"]
+            }
+        })
+
+    Test("symbol similar to neither in middle and short symbol in middle", "matc&&*hesnoth>$%ing", ["Misc A", "Misc B", {
+        Type: "Symbol Similar To Neither",
+        StartIndex: 36,
+        EndIndex: 38,
+        Symbol: "&&*"
+    }, "Misc C", "Misc D", "Misc E"], {
+            "matc": {
+                StartIndex: 32,
+                Result: ["Misc A", "Misc B"]
+            },
+            "hesnoth>$%ing": {
+                StartIndex: 39,
+                Result: ["Misc C", "Misc D", "Misc E"]
+            }
+        })
+
+    Test("symbol similar to neither in middle and long symbol in middle", "matc&&*hesnoth>$%!?ing", ["Misc A", "Misc B", {
+        Type: "Symbol Similar To Neither",
+        StartIndex: 36,
+        EndIndex: 38,
+        Symbol: "&&*"
+    }, "Misc C", "Misc D", "Misc E"], {
+            "matc": {
+                StartIndex: 32,
+                Result: ["Misc A", "Misc B"]
+            },
+            "hesnoth>$%!?ing": {
+                StartIndex: 39,
+                Result: ["Misc C", "Misc D", "Misc E"]
+            }
+        })
+
+    Test("symbol similar to neither in middle and short symbol at end", "matc&&*hesnoth>$%", ["Misc A", "Misc B", {
+        Type: "Symbol Similar To Neither",
+        StartIndex: 36,
+        EndIndex: 38,
+        Symbol: "&&*"
+    }, "Misc C", "Misc D", "Misc E"], {
+            "matc": {
+                StartIndex: 32,
+                Result: ["Misc A", "Misc B"]
+            },
+            "hesnoth>$%": {
+                StartIndex: 39,
+                Result: ["Misc C", "Misc D", "Misc E"]
+            }
+        })
+
+    Test("symbol similar to neither in middle and long symbol at end", "matc&&*hesnoth>$%!?", ["Misc A", "Misc B", {
+        Type: "Symbol Similar To Neither",
+        StartIndex: 36,
+        EndIndex: 38,
+        Symbol: "&&*"
+    }, "Misc C", "Misc D", "Misc E"], {
+            "matc": {
+                StartIndex: 32,
+                Result: ["Misc A", "Misc B"]
+            },
+            "hesnoth>$%!?": {
+                StartIndex: 39,
+                Result: ["Misc C", "Misc D", "Misc E"]
+            }
+        })
+
+    Test("symbol similar to neither at start and short symbol in middle", "&&*hesnoth>$%ing", [{
+        Type: "Symbol Similar To Neither",
+        StartIndex: 32,
+        EndIndex: 34,
+        Symbol: "&&*"
+    }, "Misc C", "Misc D", "Misc E"], {
+            "hesnoth>$%ing": {
+                StartIndex: 35,
+                Result: ["Misc C", "Misc D", "Misc E"]
+            }
+        })
+
+    Test("symbol similar to neither at start and long symbol in middle", "&&*hesnoth>$%!?ing", [{
+        Type: "Symbol Similar To Neither",
+        StartIndex: 32,
+        EndIndex: 34,
+        Symbol: "&&*"
+    }, "Misc C", "Misc D", "Misc E"], {
+            "hesnoth>$%!?ing": {
+                StartIndex: 35,
+                Result: ["Misc C", "Misc D", "Misc E"]
+            }
+        })
+
+    Test("symbol similar to neither at start and short symbol at end", "&&*hesnoth>$%", [{
+        Type: "Symbol Similar To Neither",
+        StartIndex: 32,
+        EndIndex: 34,
+        Symbol: "&&*"
+    }, "Misc C", "Misc D", "Misc E"], {
+            "hesnoth>$%": {
+                StartIndex: 35,
+                Result: ["Misc C", "Misc D", "Misc E"]
+            }
+        })
+
+    Test("symbol similar to neither at start and long symbol at end", "&&*hesnoth>$%!?", [{
+        Type: "Symbol Similar To Neither",
+        StartIndex: 32,
+        EndIndex: 34,
+        Symbol: "&&*"
+    }, "Misc C", "Misc D", "Misc E"], {
+            "hesnoth>$%!?": {
+                StartIndex: 35,
+                Result: ["Misc C", "Misc D", "Misc E"]
+            }
+        })
+
+    // todo update following
+
+    Test("short symbol at start and symbol similar to neither at middle", ">$%matches&&*nothing", [{
+        Type: "Short Symbol",
+        StartIndex: 32,
+        EndIndex: 34,
+        Symbol: ">$%"
+    }, "Misc A", "Misc B"], {
+            "matches&&*nothing": {
+                StartIndex: 35,
+                Result: ["Misc A", "Misc B"]
+            }
+        })
+
+    Test("long symbol at start and symbol similar to neither at middle", ">$%!?matches&&*nothing", [{
+        Type: "Long Symbol",
+        StartIndex: 32,
+        EndIndex: 36,
+        Symbol: ">$%!?"
+    }, "Misc A", "Misc B"], {
+            "matches&&*nothing": {
+                StartIndex: 37,
+                Result: ["Misc A", "Misc B"]
+            }
+        })
+
+    Test("short symbol at middle and symbol similar to neither at end", "matches>$%nothing&&*", ["Misc A", "Misc B", {
+        Type: "Short Symbol",
+        StartIndex: 39,
+        EndIndex: 41,
+        Symbol: ">$%"
+    }, "Misc C", "Misc D", "Misc E"], {
+            "matches": {
+                StartIndex: 32,
+                Result: ["Misc A", "Misc B"]
+            },
+            "nothing&&*": {
+                StartIndex: 42,
+                Result: ["Misc C", "Misc D", "Misc E"]
+            }
+        })
+
+    Test("long symbol at middle and symbol similar to neither at end", "matches>$%!?nothing&&*", ["Misc A", "Misc B", {
+        Type: "Long Symbol",
+        StartIndex: 39,
+        EndIndex: 43,
+        Symbol: ">$%!?"
+    }, "Misc C", "Misc D", "Misc E"], {
+            "matches": {
+                StartIndex: 32,
+                Result: ["Misc A", "Misc B"]
+            },
+            "nothing&&*": {
+                StartIndex: 44,
+                Result: ["Misc C", "Misc D", "Misc E"]
+            }
+        })
+
+    Test("short symbol at start and symbol similar to neither at end", ">$%matchesnothing&&*", [{
+        Type: "Short Symbol",
+        StartIndex: 32,
+        EndIndex: 34,
+        Symbol: ">$%"
+    }, "Misc A", "Misc B"], {
+            "matchesnothing&&*": {
+                StartIndex: 35,
+                Result: ["Misc A", "Misc B"]
+            }
+        })
+
+    Test("long symbol at start and symbol similar to neither at end", ">$%!?matchesnothing&&*", [{
+        Type: "Long Symbol",
+        StartIndex: 32,
+        EndIndex: 36,
+        Symbol: ">$%!?"
+    }, "Misc A", "Misc B"], {
+            "matchesnothing&&*": {
+                StartIndex: 37,
+                Result: ["Misc A", "Misc B"]
+            }
+        })
+
+    Test("short symbol in middle and symbol similar to neither in middle", "matc>$%hesnoth&&*ing", ["Misc A", "Misc B", {
+        Type: "Short Symbol",
+        StartIndex: 36,
+        EndIndex: 38,
+        Symbol: ">$%"
+    }, "Misc C", "Misc D", "Misc E"], {
+            "matc": {
+                StartIndex: 32,
+                Result: ["Misc A", "Misc B"]
+            },
+            "hesnoth&&*ing": {
+                StartIndex: 39,
+                Result: ["Misc C", "Misc D", "Misc E"]
+            }
+        })
+
+    Test("long symbol in middle and symbol similar to neither in middle", "matc>$%!?hesnoth&&*ing", ["Misc A", "Misc B", {
+        Type: "Long Symbol",
+        StartIndex: 36,
+        EndIndex: 40,
+        Symbol: ">$%!?"
+    }, "Misc C", "Misc D", "Misc E"], {
+            "matc": {
+                StartIndex: 32,
+                Result: ["Misc A", "Misc B"]
+            },
+            "hesnoth&&*ing": {
                 StartIndex: 41,
                 Result: ["Misc C", "Misc D", "Misc E"]
             }
