bf391b468181986c2cf38848acc2ed17d79c1897
jameswilddev <jameswilddev@outlook.com>
2017-04-07 12:41:42 +0100

    ParseTokens filename now matches its function name.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 /// <reference path="ParseUntypedTokens.ts" />
-/// <reference path="ParseToken.ts" />
+/// <reference path="ParseTokens.ts" />
 /// <reference path="ParenthesizeTokens.ts" />
 /// <reference path="ParseExpression.ts" />
