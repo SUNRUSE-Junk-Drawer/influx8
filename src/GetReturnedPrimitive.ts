7abc1f875856ea0e670f8ef05555ebc06e45d35d
jameswilddev <jameswilddev@outlook.com>
2017-04-08 14:33:00 +0100

    Added a function for getting the return primitive type of an expression.

---
+++ src/GetReturnedPrimitive.ts
@@ -0,0 +1,34 @@
+/// <reference path="TypecheckedExpression.ts" />
+/// <reference path="Primitive.ts" />
+/// <reference path="TypedUnary.ts" />
+/// <reference path="TypedBinary.ts" />
+
+function GetReturnedPrimitive(expression: TypecheckedExpression): Primitive | undefined {
+    switch (expression.Type) {
+        case "Boolean": return "Boolean"
+        case "Integer": return "Integer"
+        case "Unary": return UnaryReturnTypes[expression.Operator]
+        case "Binary": return BinaryReturnTypes[expression.Operator]
+        case "Return":
+        case "Reference":
+            return GetReturnedPrimitive(expression.Value)
+        case "Let":
+        case "LetNameNotUnique":
+        case "LetWithoutIdentifier":
+        case "LetIncorrectIdentifierType":
+            return GetReturnedPrimitive(expression.Then)
+        case "Call":
+            return GetReturnedPrimitive(expression.Result)
+        case "Unknown":
+        case "ReferenceUndefined":
+        case "CallLambdaExpected":
+        case "BinaryUnmatched":
+        case "UnaryUnmatched":
+        case "NextStatementNotFound":
+        case "Lambda":
+        case "LambdaWithoutIdentifier":
+        case "LambdaIncorrectIdentifierType":
+        case "LambdaNameNotUnique":
+            return undefined
+    }
+}
