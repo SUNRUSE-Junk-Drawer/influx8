e4e7feb53df06a7c6e75d88921ac42c442669ef9
jameswilddev <jameswilddev@outlook.com>
2017-04-21 23:28:27 +0100

    Reconfigured the project to export as a NPM module.

---
+++ src/Exports.ts
@@ -0,0 +1,19 @@
+/// <reference path="Compile.ts" />
+/// <reference path="GLSLCSyntax.ts" />
+/// <reference path="JavaScriptCSyntax.ts" />
+
+module.exports = {
+    Compile,
+    GLSLCSyntax,
+    JavaScriptCSyntax,
+    ParseUntypedTokens,
+    ParseTokens,
+    ParenthesizeTokens,
+    ParseExpression,
+    InlineExpression,
+    UnrollExpression,
+    TypecheckExpression,
+    VerifyExpression,
+    MatchCSyntax,
+    GenerateCSyntax
+}
