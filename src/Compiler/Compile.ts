2b8cf0798446f7d089e81bf2f82da63005f76c33
jameswilddev <jameswilddev@outlook.com>
2017-06-17 12:26:19 +0100

    Added a function for finding errors in the critical path of an inlined expression.

--- src/Compiler/Compile.ts
+++ src/Compiler/Compile.ts
@@ -11,7 +11,7 @@
 /// <reference path="MatchCSyntax.ts" />
 /// <reference path="GenerateCSyntax.ts" />
 /// <reference path="FindErrorsInRawExpressionCriticalPath.ts" />
-/// <reference path="FindErrorsInInlinedExpression.ts" />
+/// <reference path="FindErrorsInInlinedExpressionCriticalPath.ts" />
 
 function Compile<TUnary extends string, TBinary extends string, TFunction extends string>(source: string, syntax: CSyntax<TUnary, TBinary, TFunction>, scope: Scope): string | undefined {
     const untypedTokens = ParseUntypedTokens(source)
